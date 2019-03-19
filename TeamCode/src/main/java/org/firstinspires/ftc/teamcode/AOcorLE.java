fe04abaf0cfb2afae75b01f9aecb8bca8255b30a
EbenSteenekamp <ebenrvanheerden@gmail.com>
2019-03-19 19:32:12 +0200

    Dit werk nogsteeds nie!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AOcorLE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AOcorLE.java
@@ -3,22 +3,42 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.CloneAutoOpRobot;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.FRONT;
+
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.List;
 
 @Autonomous(name = "AOcorLE", group = "AutoOp")
 public class AOcorLE extends LinearOpMode {
-
     CloneAutoOpRobot robot = new CloneAutoOpRobot();
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
-
         waitForStart();
-
-        robot.setRobottelemetry(telemetry);
-        robot.start();
-        //unhitchRobotMoveToCorner();
-        moveToCrater();
+            robot.setRobottelemetry(telemetry);
+            robot.start();
+            //unhitchRobotMoveToCorner();
+            moveToCrater();
     }
     public void unhitchRobotMoveToCorner()
     {
